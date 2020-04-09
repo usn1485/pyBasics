68e544c5d1145aa35f6fa1502aa9f6884f2dbb4c
ujwala.nanavare <ujwala.nanavare@multiply.com>
2020-04-09 16:45:03 -0500

    Unpacking args

---
+++ unpackingArgs.py
@@ -0,0 +1,19 @@
+
+# Destructuring variable into multiple parameters.
+def add(x,y):
+    return x+y;
+
+num=[5,8]
+# '*' unpacks variablelist and assigns to each variable here. e.g x=5,y=8 
+print(add(*num))
+
+#Collecting Multiple arguments into single variable.
+#the star in function argument unpacks the data and assigns to 4 invisible variables.
+def multiply(*args):
+    print(args)  #this creates a tuple of 
+    total=1
+    for arg in args:
+        total=total*arg
+    return total
+
+print(multiply(4,6,8,6))
