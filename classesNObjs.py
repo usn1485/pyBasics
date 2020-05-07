a82a9707ea72b21bbb71bfed2e2a19e5b818a58f
ujwala.nanavare <ujwala.nanavare@multiply.com>
2020-05-07 18:38:20 -0500

    PythonObjectOriented

---
+++ classesNObjs.py
@@ -0,0 +1,24 @@
+class Store:
+    def __init__(self, name):
+        # You'll need 'name' as an argument to this method.
+        # Then, initialise 'self.name' to be the argument, and 'self.items' to be an empty list.
+        self.name = name
+        self.items = []
+
+    def add_item(self, name, price):
+        # Create a dictionary with keys name and price, and append that to self.items.
+        item = {'name': name, 'price': price}
+        self.items.append(item)
+
+    def stock_price(self):
+        total=0
+        # Add together all item prices in self.items and return the total.
+        for item in self.items:
+            total += item['price']
+        return total
+
+store=Store("Walmart")
+store.add_item('milk',3)
+store.add_item('bread',5)
+store.add_item('cheese',10)
+print(store.stock_price())
